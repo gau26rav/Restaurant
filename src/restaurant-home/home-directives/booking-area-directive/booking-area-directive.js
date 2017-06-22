92f24625e6a706ae8bf8b46cb90dbb48c538cc3a
Vikash1.Kumar <kumar.vikash561@gmail.com>
2017-06-22 13:57:59 +0530

    added booking -area directive to home

---
+++ src/restaurant-home/home-directives/booking-area-directive/booking-area-directive.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Vikash1.Kumar on 22-06-2017.
+ */
+(function () {
+    angular.module("indexContentModule").directive("bookingArea", function () {
+        return{
+            templateUrl :"src/restaurant-home/home-directives/booking-area-directive/booking-area-view.html"
+        }
+    })
+})();
